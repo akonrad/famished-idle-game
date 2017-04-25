6f47a4bf8404be53731f4c8ce68a4b61234c9d11
Ruellia Zhang <syncyes@gmail.com>
2017-04-24 21:53:56 -0700

    slightly prettier stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Panel} from 'react-bootstrap';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class Stats extends React.Component {
@@ -10,10 +11,18 @@ export default class Stats extends React.Component {
     }
 
     render() {
-        return <ul>
-            <li>body: {this.props.body}</li>
-            <li>mind: {this.props.mind}</li>
-            <li>soul: {this.props.soul}</li>
-        </ul>
+        return <div>
+            <Panel
+                header="Stats"
+                collapsible={true}
+                defaultExpanded={true}
+                bsStyle="primary">
+                <ul>
+                    <li>body: {this.props.body}</li>
+                    <li>mind: {this.props.mind}</li>
+                    <li>soul: {this.props.soul}</li>
+                </ul>
+            </Panel>
+        </div>
     }
 };
